a19be02ae27d64406981f7d5b04b03e690774678
Abhi <professorkevin786@gmail.com>
2025-05-02 17:39:16 +0530

    j

--- src/app/component/pipe/pipe.component.ts
+++ src/app/component/pipe/pipe.component.ts
@@ -1,13 +1,34 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe, I18nPluralPipe, I18nSelectPipe, KeyValuePipe, NgFor, PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
-  imports: [DatePipe],
+  imports: [DecimalPipe, PercentPipe, I18nPluralPipe, I18nSelectPipe,KeyValuePipe,NgFor],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.scss'
 })
 export class PipeComponent {
 
-  today = new Date()
+  num = 111;
+
+  messageCount = 9;
+  messages = {
+    '=0': 'No messages',
+    '=1': 'One message',
+    'other': '# messages'
+  };
+
+  gender = "male";
+  genderMap = {
+    'male': 'Invite him.',
+    'female': 'Invite her.',
+    'other': 'Invite them.'
+  };
+
+  user = {
+    name: 'Alice',
+    age: 28,
+    role: 'Developer'
+  };
+
 }
